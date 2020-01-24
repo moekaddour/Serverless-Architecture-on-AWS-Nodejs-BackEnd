746817b2bb8914277e7c892fae213528dca68d56
Moe Kaddour <m.kaddour26@gmail.com>
2020-01-23 19:04:46 -0500

    config file is edited

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 module.exports = {
-    consumer_key: "QIYwp15wjOyQoTQzrD19YaDuR",
-    consumer_secret: "YgWgDEhMkHmrJsxtB0smmorHuf6CJT5vBCjQYlJdO0Gnz2SC5Q",
-    access_token: "1184990840810328065-CpQ7a2s3ahIcK6rsWJzMWiuN2k3xhY",
-    access_token_secret: "UQWLLJNmxa9UjWQ3rfy2yYoXe92jAyIVqiAhGUG1T7ruY",
+    consumer_key: "add you own credentials",
+    consumer_secret: "add you own credentials",
+    access_token: "add you own credentials",
+    access_token_secret: "add you own credentials",
    // timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
    // strictSSL: true // optional - requires SSL certificates to be valid.
   };
   
+  // You Get these credentials from AWS accounts
